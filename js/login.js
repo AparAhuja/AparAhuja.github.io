129b2367bc688ed2a8ba70c438d0cb128cf0b229
Apar Ahuja <aparahuja@Apars-MacBook-Air.local>
2022-12-25 11:22:30 +0530

    update

---
+++ js/login.js
@@ -0,0 +1,22 @@
+// Validate the form submission
+function validateForm() {
+  // Get the username and password
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Check if the username and password are correct
+  if (username === "admin" && password === "password") {
+
+    // Redirect to the CGPA calculator page
+    window.location.href = "cgpa.html";
+
+    // Prevent the form from being submitted
+    return false;
+  } else {
+    // Show an error message
+    alert("Incorrect username or password");
+
+    // Prevent the form from being submitted
+    return false;
+  }
+}
